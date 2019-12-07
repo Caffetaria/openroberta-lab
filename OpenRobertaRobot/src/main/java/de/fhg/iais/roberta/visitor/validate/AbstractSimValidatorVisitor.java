a4a7dffa3b85c2ed741fd3746708edb308621d6f
Caffetaria <ariavikram@gmail.com>
2019-12-07 08:27:25 +0530

    Fix wrong calliope error handling for ultrasonic sensor

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/visitor/validate/AbstractSimValidatorVisitor.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/visitor/validate/AbstractSimValidatorVisitor.java
@@ -155,6 +155,15 @@ public abstract class AbstractSimValidatorVisitor extends AbstractProgramValidat
         }
         return null;
     }
+    
+    @Override
+    public Void visitUltrasonicSensor(UltrasonicSensor<Void> ultrasonicSensor) {
+        super.visitUltrasonicSensor(ultrasonicSensor);
+        if ( ultrasonicSensor.getMode().toString().equals("AMBIENTLIGHT") ) { {
+            ultrasonicSensor.addInfo(NepoInfo.warning("SIM_BLOCK_NOT_SUPPORTED"));
+        }
+        return null;
+    }
 
     @Override
     public Void visitRepeatStmt(RepeatStmt<Void> repeatStmt) {
